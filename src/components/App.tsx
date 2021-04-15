7413a304b17ef281a32cd02bb2d3cd4a089d6627
mrutyunjay <mrutyunjay.swain@gmail.com>
2021-04-15 21:09:16 +0800

    initial commits for my react app

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Message from "./Message";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>This is react in action</h2>
+      <Message message="hello"></Message>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Message.tsx
@@ -0,0 +1,5 @@
+const Message = (props: { message: string }) => {
+  return <div>My message is: {props.message}</div>;
+};
+
+export default Message;
