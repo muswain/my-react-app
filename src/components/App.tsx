7e00101ebe46395b91c3118914df514a71fba5b9
mrutyunjay <mrutyunjay.swain@gmail.com>
2021-04-15 21:49:06 +0800

    added material-ui to react app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Message from "./Message";
 
 const App = () => {
+  const initialProps = { message: "hello", value: 5 };
+
   return (
     <div className="App">
       <h2>This is react in action</h2>
-      <Message message="hello"></Message>
+      <Message {...initialProps}></Message>/
     </div>
   );
 };
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,41 @@
-const Message = (props: { message: string }) => {
-  return <div>My message is: {props.message}</div>;
+import { useState } from "react";
+import { Button, makeStyles } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+interface MessageProps {
+  message: string;
+  value: number;
+}
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const Message = (props: MessageProps) => {
+  const classes = useStyles();
+  const [count, setCount] = useState(props.value);
+  const [message, setMessage] = useState(props.message);
+
+  return (
+    <div>
+      <h2>Message is: {message}</h2>
+      <h2>Clicked count is: {count}</h2>
+      <Button
+        color="primary"
+        variant="contained"
+        className={classes.button}
+        startIcon={<DeleteIcon />}
+        onClick={() => {
+          setCount(count + 1);
+          setMessage(`counter being incremented to: ${count}`);
+        }}
+      >
+        Click to Increment
+      </Button>
+    </div>
+  );
 };
 
 export default Message;
